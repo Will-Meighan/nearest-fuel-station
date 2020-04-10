bb0a2c40f3562a41570a44087417912e805fb5d8
William Meighan <williamdmeighan@gmail.com>
2020-04-10 09:50:02 -0600

    create NREL service

---
+++ app/services/nrel_service.rb
@@ -0,0 +1,24 @@
+class NrelService
+  def initialize(location)
+    @location = location
+  end
+
+  def get_station
+    get_json
+  end
+
+  private
+
+    def conn
+      Faraday.new("https://developer.nrel.gov//api/alt-fuel-stations/v1/nearest.json") do |faraday|
+        faraday.params['api_key'] = ENV['NREL_KEY']
+        faraday.params['location'] = @location
+        faraday.adapter Faraday.default_adapter
+      end
+    end
+
+    def get_json
+      response = conn.get
+      JSON.parse(response.body, symbolize_names: true)
+    end
+end
